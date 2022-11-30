daf5ac979da738f401cbc82267bd02a295d794bc
GabrielHenriqueM <119533415+GabrielHenriqueM@users.noreply.github.com>
2022-11-30 16:23:37 -0300

    Add files via upload

---
+++ funcao.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+double maior(int a, int b){
+
+
+    if(a > b){
+        return a;
+
+    }else{
+        return b;
+
+    }
+
+
+}
+
+
+
+int main(){
+    int a, b, maior;
+
+   scanf("%d", &a);
+   scanf("%d", &b);
+
+   maior = (a, b);
+
+   printf("%d", maior);
+
+
+    return 0;
+}
+
