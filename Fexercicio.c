f164b7df68293ae14ba7ae9d171abb435112c5ec
GabrielHenriqueM <119533415+GabrielHenriqueM@users.noreply.github.com>
2022-11-30 22:44:03 -0300

    Add files via upload

---
+++ Fexercicio.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int qntPares(int a, int b, int c){
+
+    int cont;
+
+    if(a % 2 == 0){
+        cont++;
+
+    } if(b % 2 == 0){
+        cont++;
+
+    } if(c % 2 == 0){
+        cont++;
+    }
+
+    return cont;
+
+}
+
+int main() {
+
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("%d", qntPares(a, b, c));
+
+
+
+	return 0;
+}
